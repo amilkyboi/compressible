639225434d01340390f3bb31b727391aafdb9f81
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2023-08-17 12:57:11 -0500

    add tabs, modular design

--- src/app.py
+++ src/app.py
@@ -7,7 +7,8 @@ import sys
 import math
 
 from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QComboBox, \
-                            QPushButton, QLabel, QLineEdit, QApplication, QMessageBox
+                            QPushButton, QLabel, QLineEdit, QApplication, QMessageBox, \
+                            QTabWidget
 
 import isentropic as isen
 import inverse as inv
@@ -15,23 +16,28 @@ import inverse as inv
 # Prandtl-Meyer angle limit in [deg]
 PM_ANGLE_LIMIT = 130.454076
 
-class CompressibleApp(QMainWindow):
+class CalculatorTab(QWidget):
     '''
-    Calculator.
+    _summary_
+
+    Args:
+        QWidget (_type_): _description_
     '''
 
-    def __init__(self):
+    def __init__(self, inputs, outputs):
         super().__init__()
+        self.init_ui(inputs, outputs)
 
-        # WINDOW -----------------------------------------------------------------------------------
+    def init_ui(self, input_list, output_list):
+        '''
+        _summary_
 
-        self.setWindowTitle('Compressible Flow Calculator')
-        self.setGeometry(100, 100, 500, 300)
+        Args:
+            input_list (_type_): _description_
+            output_list (_type_): _description_
+        '''
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.layout = QVBoxLayout() # type: ignore
+        self.tab_layout = QVBoxLayout()
 
         # GAMMA ------------------------------------------------------------------------------------
 
@@ -41,18 +47,15 @@ class CompressibleApp(QMainWindow):
         self.gamma_layout.addWidget(QLabel('Gamma:'))
         self.gamma_layout.addWidget(self.gamma_ledit)
 
-        self.layout.addLayout(self.gamma_layout)
+        self.tab_layout.addLayout(self.gamma_layout)
 
         # DROPDOWN ---------------------------------------------------------------------------------
 
         self.dropdown = QComboBox()
 
-        quantities = ['Mach number', 'T/T0', 'p/p0', 'rho/rho0', 'A/A* (subsonic)', \
-                      'A/A* (supersonic)', 'Mach angle (deg)', 'P-M angle (deg)']
+        self.dropdown.addItems(input_list)
 
-        self.dropdown.addItems(quantities)
-
-        self.layout.addWidget(self.dropdown)
+        self.tab_layout.addWidget(self.dropdown)
 
         # INPUT ------------------------------------------------------------------------------------
 
@@ -62,14 +65,14 @@ class CompressibleApp(QMainWindow):
         self.input_layout.addWidget(QLabel('Input:'))
         self.input_layout.addWidget(self.input_ledit)
 
-        self.layout.addLayout(self.input_layout)
+        self.tab_layout.addLayout(self.input_layout)
 
         # CALCULATION BUTTON -----------------------------------------------------------------------
 
         self.calculate_button = QPushButton('Calculate')
         self.calculate_button.clicked.connect(self.calculate)
 
-        self.layout.addWidget(self.calculate_button)
+        self.tab_layout.addWidget(self.calculate_button)
 
         # RESULTS ----------------------------------------------------------------------------------
 
@@ -77,8 +80,7 @@ class CompressibleApp(QMainWindow):
         self.result_labels = []
         self.result_ledits = []
 
-        for operation in ['Mach number', 'Mach angle (deg)', 'P-M angle (deg)', 'p/p0', \
-                          'rho/rho0', 'T/T0', 'p/p*', 'rho/rho*', 'T/T*', 'A/A*']:
+        for operation in output_list:
             result_label = QLabel(f'{operation}:')
             result_ledit = QLineEdit()
             result_ledit.setReadOnly(True)
@@ -86,11 +88,11 @@ class CompressibleApp(QMainWindow):
             self.result_labels.append(result_label)
             self.result_ledits.append(result_ledit)
 
-            self.layout.addWidget(result_label)
-            self.layout.addWidget(result_ledit)
+            self.tab_layout.addWidget(result_label)
+            self.tab_layout.addWidget(result_ledit)
 
-        self.central_widget.setLayout(self.layout)
-
+        self.setLayout(self.tab_layout)
+    
     # CALL CALCULATION FUNCTIONS -------------------------------------------------------------------
 
     def calculate(self):
@@ -209,8 +211,6 @@ class CompressibleApp(QMainWindow):
 
         return gamma, mach_num, flow_type
 
-    # ERROR POPUP ----------------------------------------------------------------------------------
-
     def show_error_popup(self, err_msg):
         '''
         _summary_
@@ -226,6 +226,42 @@ class CompressibleApp(QMainWindow):
         error_popup.setStandardButtons(QMessageBox.StandardButton.Ok)
         error_popup.exec()
 
+class CompressibleApp(QMainWindow):
+    '''
+    Calculator.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        '''
+        _summary_
+        '''
+
+        self.setWindowTitle('Compressible Flow Calculator')
+        self.setGeometry(100, 100, 500, 300)
+
+        self.tab_widget = QTabWidget()
+        self.setCentralWidget(self.tab_widget)
+
+        # Create and add tabs
+        isen_inputs  = ['Mach number', 'T/T0', 'p/p0', 'rho/rho0', 'A/A* (subsonic)', \
+                       'A/A* (supersonic)', 'Mach angle (deg)', 'P-M angle (deg)']
+        isen_outputs = ['Mach number', 'Mach angle (deg)', 'P-M angle (deg)', 'p/p0', 'rho/rho0', \
+                       'T/T0', 'p/p*', 'rho/rho*', 'T/T*', 'A/A*']
+        self.tab_widget.addTab(CalculatorTab(isen_inputs, isen_outputs), 'Isentropic Relations')
+
+        norm_inputs  = ['M1', 'M2', 'p2/p1', 'rho2/rho1', 'T2/T1', 'p02/p01', 'p1/p02']
+        norm_outputs = norm_inputs
+        self.tab_widget.addTab(CalculatorTab(norm_inputs, norm_outputs), 'Normal Shock Relations')
+
+        oblq_inputs  = ['Turn angle (weak)', 'Turn angle (strong)', 'Wave angle', 'M1n']
+        oblq_outputs = ['M2', 'Turn angle', 'Wave angle', 'p2/p1', 'rho2/rho1', 'T2/T1', \
+                        'p02/p01', 'M1n', 'M2n']
+        self.tab_widget.addTab(CalculatorTab(oblq_inputs, oblq_outputs), 'Oblique Shock Relations')
+
 def main():
     '''
     _summary_
@@ -233,6 +269,7 @@ def main():
 
     app    = QApplication(sys.argv)
     window = CompressibleApp()
+    app.setStyle('fusion')
     window.show()
     sys.exit(app.exec())
 
